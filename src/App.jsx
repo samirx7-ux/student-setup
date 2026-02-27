0634e2efaef7972a56bc80d9a69c876b83b75035
samirx7-ux <sameerxahad@gmail.com>
2026-02-27 11:59:37 +0530

    Wrap app in UserProvider for context management

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './styles/global.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
 import AppShell from './components/AppShell';
 import HomeView from './views/HomeView';
 import ProgramsView from './views/ProgramsView';
@@ -12,21 +13,23 @@ import SkillDetail from './views/SkillDetail';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppShell />}>
-          <Route index element={<Navigate to="/home" replace />} />
-          <Route path="/home" element={<HomeView />} />
-          <Route path="/programs" element={<ProgramsView />} />
-          <Route path="/programs/:id" element={<ProgramDetail />} />
-          <Route path="/universities" element={<UniversitiesView />} />
-          <Route path="/universities/:id" element={<UniversityDetail />} />
-          <Route path="/skills" element={<SkillsView />} />
-          <Route path="/skills/:id" element={<SkillDetail />} />
-          <Route path="/profile" element={<ProfileView />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppShell />}>
+            <Route index element={<Navigate to="/home" replace />} />
+            <Route path="/home" element={<HomeView />} />
+            <Route path="/programs" element={<ProgramsView />} />
+            <Route path="/programs/:id" element={<ProgramDetail />} />
+            <Route path="/universities" element={<UniversitiesView />} />
+            <Route path="/universities/:id" element={<UniversityDetail />} />
+            <Route path="/skills" element={<SkillsView />} />
+            <Route path="/skills/:id" element={<SkillDetail />} />
+            <Route path="/profile" element={<ProfileView />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   );
 }
 
